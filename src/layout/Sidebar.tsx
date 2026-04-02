116e86e659e18af75f599a11db21a8e371804869
haddjs <nadirhexonyte@gmail.com>
2026-04-02 09:07:53 +0700

    feat: initial commit

---
+++ src/layout/Sidebar.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+const Sidebar = () => {
+  return (
+    <div>
+      <Image src="/logo-white.svg" alt="Logo" width={80} height={80} />
+    </div>
+  );
+};
+
+export default Sidebar;
